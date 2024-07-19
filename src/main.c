9a6658a86f955e4bd85c38b2024221651a1034aa
ORMAN <21mk386@esisalama.org>
2024-07-19 08:54:19 +0200

    Définition des tables et des champs

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include "table.h"
-#include "data.h"
-#include "requete.h"
+#include "../data/data.h"
+#include "../requete/requete.h"
 
 int main() {
     return 0;
